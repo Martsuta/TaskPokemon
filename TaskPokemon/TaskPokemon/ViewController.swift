89ddf7d4eedaccdab2b2539e2e60ba83a2a4f4e1
Irina Martsuta <irishenka14@yandex.ru>
2023-04-20 19:04:53 +0300

    Настройка и удаление Main сториборда

---
+++ TaskPokemon/TaskPokemon/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  TaskPokemon
+//
+//  Created by Ирина Марцута on 20.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
